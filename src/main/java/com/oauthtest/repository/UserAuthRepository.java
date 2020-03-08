8a9fbf6dbb1aed5807ed9ef0dd904b6e5f075eb4
WJKIM <kfship@gmail.com>
2020-03-08 23:49:29 +0900

    @RepositoryRestResource 설정 변경

--- src/main/java/com/oauthtest/repository/UserAuthRepository.java
+++ src/main/java/com/oauthtest/repository/UserAuthRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 //@Repository
-@RepositoryRestResource(path = "userInfo", excerptProjection = UsersProjection.class)
+@RepositoryRestResource(collectionResourceRel  = "collection", itemResourceRel = "item", path = "userInfo", excerptProjection = UsersProjection.class)
 public interface UserAuthRepository extends PagingAndSortingRepository<User, String>, JpaSpecificationExecutor<User>  {
 
     User findByUserId(String userId);
